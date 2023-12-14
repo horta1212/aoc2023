f15ed1943aa158a59c037935aaf8884eb8de0f8e
horta1212 <horta1212@gmail.com>
2023-12-14 00:41:48 -0600

    d14

---
+++ test.py
@@ -0,0 +1,66 @@
+
+'''
+steps = [20,45,65]
+max_steps = max(steps)
+min_steps = min(steps)
+
+lcm = min_steps
+while True:
+    lcm += min_steps 
+    j=0
+    while j < len(steps):
+        print('j',j)
+        if lcm % steps[j] != 0:
+            print('lcm',lcm)
+            break
+        j += 1
+
+    print(j)
+    if j==len(steps):
+        break
+
+print('lcm',lcm)
+'''
+from bryces_utils import Timer
+from math import sqrt
+def prime_factors(num):
+    i = 1
+    factor_list=[]
+    primes_list=[]
+    while num > 1:
+        i+=1
+        if num % i == 0:
+            factor_list.append(i)
+            num = num/i
+            print(num)
+            i=1
+
+    print(factor_list)
+    return(factor_list)
+
+with Timer('sqrt_factors'):
+    prime_factors(1e24)
+
+def prime_numbers(num):
+    prime_list=[2]
+    for i in range(3,int(num)):
+        prime=True
+        for prime in prime_list:
+            if i % prime == 0:
+                prime=False
+                break
+        if prime:
+            prime_list.append(i)
+        if i % (num/100) == 0:
+            print('i',i)
+            print(prime_list)
+    return(prime_list)
+
+        
+
+        
+
+
+
+
+
